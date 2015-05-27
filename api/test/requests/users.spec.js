143dc41de42fa78618e4575761d997662575d2d9
Kyle Davis <validkeys@gmail.com>
2015-05-27 16:25:54 -0400

    got a test running

--- api/test/requests/users.spec.js
+++ api/test/requests/users.spec.js
@@ -1,35 +1,41 @@
 var Lab       = require('lab'),
-    lab       = exports.lab = Lab.script(),
+    Code      = require('code'),
     Glue      = require('glue'),
     reqlib    = require('app-root-path').require,
     AppConfig = reqlib('/app/config');
 
 global.reqlib   = require('app-root-path').require;
 
+// shortcuts
+var lab         = exports.lab = Lab.script(),
+    expect      = Code.expect
+    beforeEach  = lab.beforeEach,
+    before      = lab.before;
+
 var glueOptions = {
   relativeTo: require('app-root-path').resolve('/app')
 };
 
-Glue.compose(AppConfig.manifest, glueOptions, function(err, server) {
+var server = null;
 
-  if (err){ console.log("Glue Error!", err); }
+lab.experiment('Users', function() {
 
-  server.start(function() {
-    
-    lab.experiment('Users', function() {
-      lab.test('user get endpoint works', function(done) {
-
-        var options = { method: "GET", url: "/users/761e7a03-c731-4d5c-918a-ed8690cf32f2" };
-
-        server.inject(options, function(response) {
-          var result = response.result;
-          lab.expect(result.status).to.equal(200);
-          done();
-        });
-
+  before(function(done) {
+    Glue.compose(AppConfig.manifest, glueOptions, function(err, serverInstance) {
+      if (err){ console.log("Glue Error!", err); }
+      server = serverInstance;
+      server.start(function(err) {
+        done();
       });
     });
+  });
 
+  lab.test('user get endpoint works', function(done) {
+    var options = { method: "GET", url: "/users/761e7a03-c731-4d5c-918a-ed8690cf32f2" };
+    server.inject(options, function(response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
 });
