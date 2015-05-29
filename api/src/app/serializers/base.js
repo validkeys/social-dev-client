a7da722b2894286c1ba6e8ae331304fe41010319
Kyle Davis <validkeys@gmail.com>
2015-05-28 20:36:05 -0400

    added a TODO

--- api/src/app/serializers/base.js
+++ api/src/app/serializers/base.js
@@ -1,13 +1,16 @@
 import _ from 'lodash';
 
+// TODO: would be nice to have a _private field
+// in the constructor where the final data is
+// right now the final data is potentially in several different places
+// a singular FINAL_DATA var that gets built by the serializer
+// might make this easier to reason about.
+
 class BaseSerialzer {
   constructor(data = {}) {
     this.data       = data;
     this.isArray    = _.isArray(this.data);
     this.fields     = '*';
-
-    // Don't auto run if in test mode
-    let env = process.env.NODE_ENV;
   }
 
   serialize() {
@@ -39,8 +42,8 @@ class BaseSerialzer {
     let fields      = this.fields
     let dataObject  = _.pick(data, fields);
 
-    let existingKeys = _.keys(data);
-    let virtualKeys = _.difference(fields, existingKeys);
+    let existingKeys  = _.keys(data);
+    let virtualKeys   = _.difference(fields, existingKeys);
 
     if (virtualKeys.length) {
       virtualKeys.forEach((key) => {
