546b6f554c7e3ed666dbb75c6c5d7cc888e5e265
Kyle Davis <validkeys@gmail.com>
2015-06-03 15:33:32 -0400

    Still working on session initializer

--- client/app/instance-initializers/session.js
+++ client/app/instance-initializers/session.js
@@ -1,11 +1,15 @@
-import Session from 'simple-auth/session';
 import Ember from 'ember';
 
-export function initialize(instance) {
-  Session.reopen({
+export function initialize(application) {
+  console.log("Initializing!!!");
+  var session = application.container.lookup("simple-auth-session:main"),
+      store   = application.container.lookup("store:main");
+  
+  session.reopen({
     currentUser:    null,
     tokenData:      {},
     setCurrentUser: function(){
+      console.log("HELLO!");
       var token     = this.get('token'),
           tokenData = (token && token !== null) ? JSON.parse(atob(this.get('token').split('.')[1])) : null;
 
@@ -18,11 +22,11 @@ export function initialize(instance) {
 
       Ember.run(this, function() {
         console.log("Fetching user...");
-        var appIsReady = instance.container.lookup("controller:application").get("isReady");
+        var appIsReady = application.container.lookup("controller:application").get("isReady");
         if (!appIsReady) {
           application.deferReadiness();
         }
-        instance.container.lookup('store:main')
+        store
           .find('user', tokenData.user.id)
           .then(function(user) {
             _this.set("currentUser", user);
@@ -36,12 +40,11 @@ export function initialize(instance) {
             }
           });
         });
-    }.observes('isAuthenticated').on('init')
+    }.observes('token').on('init')
   });
 }
 
 export default {
   name:       'session',
-  // before:     'simple-auth',
   initialize: initialize
 };
